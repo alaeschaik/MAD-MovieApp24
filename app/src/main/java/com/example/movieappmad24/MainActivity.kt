2bc09e52c6be8e779f51cf84fe1a11e76225c632
Ali Sheikh <ali.sheikh@k-business.com>
2024-02-26 18:14:20 +0100

    Add Top and BottomAppBar

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -4,18 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,9 +43,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Card {
-                        Image(painter = painterResource(id = R.drawable.movie_image), contentDescription = "placeholder_image")
+                    Column {
+                        TopAppBar(title = {
+                            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                                Text(text = "Movie App")
+                            }})
+                        Card {
+                            Image(painter = painterResource(id = R.drawable.movie_image), contentDescription = "placeholder_image")
+                        }
+                        Spacer(modifier = Modifier.weight(1f))
+                        BottomAppBar {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceEvenly
+                            ) {
+                                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                    IconButton(onClick = { /* Handle action */ }) {
+                                        Icon(Icons.Filled.Home, contentDescription = "Home")
+                                    }
+                                    Text("Home")
+                                }
+                                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(Icons.Filled.Star, contentDescription = "Watchlist")
+                                    }
+                                    Text("Watchlist")
+                                }
+                            }
+                        }
                     }
+
                 }
             }
         }
