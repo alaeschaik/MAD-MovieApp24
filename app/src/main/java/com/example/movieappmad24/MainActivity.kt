1dfe594a384457b748709ea70c6b72f9db0b8c5f
Ali Sheikh <ali@sheikhs.at>
2024-02-29 17:09:57 +0100

    Add movie cards details

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -1,29 +1,36 @@
 package com.example.movieappmad24
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.KeyboardArrowUp
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -33,10 +40,13 @@ import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -72,7 +82,7 @@ class MainActivity : ComponentActivity() {
                                 horizontalArrangement = Arrangement.SpaceEvenly
                             ) {
                                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                                    IconButton(onClick = { /* Handle action */ }) {
+                                    IconButton(onClick = { /*TODO*/ }) {
                                         Icon(Icons.Filled.Home, contentDescription = "Home")
                                     }
                                     Text("Home")
@@ -109,45 +119,86 @@ fun MovieList(movies: List<Movie>) {
 
 @Composable
 fun MovieRow(movie: Movie) {
+    var showDetails by remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(5.dp),
-        shape = ShapeDefaults.Large
+        shape = ShapeDefaults.Large,
+        elevation = CardDefaults.cardElevation(10.dp)
     ) {
-        Column(Modifier.background(MaterialTheme.colorScheme.secondary)) {
-            Box {
+        Column {
+            Box(
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
                 AsyncImage(
                     model = ImageRequest.Builder(LocalContext.current)
                         .data(movie.images.first())
-                        .crossfade(true)
                         .build(),
                     contentDescription = "Movie Image",
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
-                        .background(MaterialTheme.colorScheme.background)
+                    contentScale = ContentScale.Crop,
                 )
-
-                IconButton(onClick = { /*TODO*/ }, Modifier.align(Alignment.TopEnd)) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(10.dp),
+                    contentAlignment = Alignment.TopEnd
+                ) {
                     Icon(
-                        Icons.Outlined.FavoriteBorder,
-                        contentDescription = "FavouriteBorder",
-                        tint = Color.White
+                        tint = MaterialTheme.colorScheme.secondary,
+                        imageVector = Icons.Default.FavoriteBorder,
+                        contentDescription = "Add to favorites"
                     )
                 }
             }
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .size(40.dp)
-                    .background(Color.LightGray)
-                    .padding(start = 10.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(text = movie.title)
-                Icon(Icons.Outlined.KeyboardArrowUp, contentDescription = "Up")
+                Icon(
+                    modifier = Modifier
+                        .clickable {
+                            showDetails = !showDetails
+                        },
+                    imageVector =
+                    if (showDetails) Icons.Filled.KeyboardArrowDown
+                    else Icons.Default.KeyboardArrowUp, contentDescription = "show more"
+                )
             }
+            MovieDetail(showDetails, movie)
+        }
+    }
+}
+
+@Composable
+private fun MovieDetail(
+    showMovieDetails: Boolean,
+    movie: Movie
+) {
+    AnimatedVisibility(
+        visible = showMovieDetails,
+        enter = expandVertically(expandFrom = Alignment.Top) { 20 },
+        exit = shrinkVertically(shrinkTowards = Alignment.Top) { 20 }) {
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(text = "Director: ${movie.director}")
+            Text(text = "Released: ${movie.year}")
+            Text(text = "Genre: ${movie.genre}")
+            Text(text = "Actors: ${movie.actors}")
+            Text(text = "Rating: ${movie.rating}")
+
+            Divider(modifier = Modifier.padding(4.dp))
+
+            Text(text = "Plot: ${movie.plot}")
         }
     }
 }
