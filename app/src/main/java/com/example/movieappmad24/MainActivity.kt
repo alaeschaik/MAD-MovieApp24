c4e1f4ccdab4aafd95f328ff9a13f38e5bd616da
Ali Sheikh <ali.sheikh@k-business.com>
2024-02-29 15:57:45 +0100

    WIP

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -1,35 +1,46 @@
 package com.example.movieappmad24
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.KeyboardArrowUp
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
 class MainActivity : ComponentActivity() {
@@ -39,19 +50,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             MovieAppMAD24Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column {
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background),
+                    topBar = {
                         TopAppBar(title = {
-                            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
                                 Text(text = "Movie App")
-                            }})
-                        Card {
-                            Image(painter = painterResource(id = R.drawable.movie_image), contentDescription = "placeholder_image")
-                        }
-                        Spacer(modifier = Modifier.weight(1f))
+                            }
+                        })
+                    },
+                    bottomBar = {
                         BottomAppBar {
                             Row(
                                 modifier = Modifier.fillMaxWidth(),
@@ -72,8 +85,69 @@ class MainActivity : ComponentActivity() {
                             }
                         }
                     }
-
+                )
+                {
+                    Column(Modifier.padding(it))
+                    {
+                        MovieList(movies = Movie.getMovies())
+                    }
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieList(movies: List<Movie>) {
+    LazyColumn {
+        items(movies) { movie ->
+            MovieRow(movie = movie)
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: Movie) {
+    val request = ImageRequest.Builder(context)
+        .data("https://example.com/image.jpg")
+        .build()
+    val drawable = imageLoader.execute(request).drawable
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp),
+        shape = ShapeDefaults.Large
+    ) {
+        Column(Modifier.background(MaterialTheme.colorScheme.secondary)) {
+            Box {
+                AsyncImage(
+                    model = movie.images.first(),
+                    contentDescription = "Movie Image",
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+                        .background(MaterialTheme.colorScheme.background)
+                )
+
+                IconButton(onClick = { /*TODO*/ }, Modifier.align(Alignment.TopEnd)) {
+                    Icon(
+                        Icons.Outlined.FavoriteBorder,
+                        contentDescription = "FavouriteBorder",
+                        tint = Color.White
+                    )
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(40.dp)
+                    .background(Color.LightGray)
+                    .padding(start = 10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = movie.title)
+                Icon(Icons.Outlined.KeyboardArrowUp, contentDescription = "Up")
             }
         }
     }
